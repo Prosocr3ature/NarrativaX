cf93f699ed752c56b7b1c4aebb37e4c88905d763
Prosocr3ature <35155025+Prosocr3ature@users.noreply.github.com>
2025-04-17 22:10:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__, static_folder='public', static_url_path='/public')
+app = Flask(__name__, static_folder="public")
 
-@app.route('/')
-def index():
-    return send_from_directory('public', 'index.html')
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory("public", path)
 
-@app.route('/manifest.json')
-def manifest():
-    return send_from_directory('public', 'manifest.json')
-
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory('public', path)
+@app.route("/")
+def home():
+    return "NarrativaX Flask Backend Running"
