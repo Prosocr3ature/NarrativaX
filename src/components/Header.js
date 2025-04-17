d1a1b66cc5277b2c6b39336e2dedc48bfc38d26b
Prosocr3ature <35155025+Prosocr3ature@users.noreply.github.com>
2025-04-18 00:03:04 +0200

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>NarrativaX</h1>
+      <p>Your AI-powered book studio</p>
+    </header>
+  );
+}
+
+export default Header;
